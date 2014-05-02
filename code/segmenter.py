fdb29494117365c1a805350a182f83ea8bec59b5
Brian McFee <brm2132@columbia.edu>
2014-05-02 18:17:09 -0400

    rolled back normalization

--- code/segmenter.py
+++ code/segmenter.py
@@ -249,7 +249,7 @@ def do_segmentation(X, beats, parameters):
     Xs = librosa.segment.stack_memory(X, n_steps=N_STEPS)
 
     # Normalize: equivalent to cosine-similarity
-    Xs = librosa.util.normalize(Xs, norm=2)
+#     Xs = librosa.util.normalize(Xs, norm=2)
 
     k_link = 1 + int(np.ceil(np.log2(X.shape[1])))
     R = librosa.segment.recurrence_matrix(  Xs, 
