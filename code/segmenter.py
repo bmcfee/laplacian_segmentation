a695974eda4adfa2a052874e41c90f39a4263af9
Brian McFee <brm2132@columbia.edu>
2014-05-02 15:12:30 -0400

    remove l2 normalization

--- code/segmenter.py
+++ code/segmenter.py
@@ -251,7 +251,7 @@ def do_segmentation(X, beats, parameters):
     Xs = librosa.segment.stack_memory(Xpad, n_steps=N_STEPS)[:, N_STEPS:]
 
     # Normalize: equivalent to cosine-similarity
-    Xs = librosa.util.normalize(Xs, norm=2)
+#     Xs = librosa.util.normalize(Xs, norm=2)
 
     k_link = 1 + int(np.ceil(np.log2(X.shape[1])))
     R = librosa.segment.recurrence_matrix(  Xs, 
