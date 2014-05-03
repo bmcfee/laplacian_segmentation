48faaa53245cdb13ed59220be961b76010e7df00
Brian McFee <brm2132@columbia.edu>
2014-05-03 16:55:22 -0400

    derp, score was upside-down

--- code/segmenter.py
+++ code/segmenter.py
@@ -279,7 +279,8 @@ def label_clusterer(Lf, k_min, k_max):
         c2 = cond_entropy(label_dict[n_types-1], labels) / np.log(n_types-1 + 1e-12)
 
         # take the harmonic mean
-        score = mir_eval.util.f_measure(1-c1, 1-c2)
+        # negate: we want to minimize s_f across levels
+        score = - mir_eval.util.f_measure(1-c1, 1-c2)
 
         if score > best_score and len(boundaries) > k_min:
             best_boundaries = boundaries
